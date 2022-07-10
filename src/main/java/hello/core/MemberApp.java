dea5a90a324b5ad3385f6800b6169604ebd0b574
JSH <suk05088@gmail.com>
2022-07-10 16:09:42 +0900

    spring ac refactoring

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,17 +4,26 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+        // AppConfig appConfig = new AppConfig();
 
         // MemberService memberService = new MemberServiceImpl();
         // AppConfig로 전환
 
-        MemberService memberService = appConfig.memberService();
-        
+        // MemberService memberService = appConfig.memberService();
+
+        /**
+         * Spring 도입
+         */
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
